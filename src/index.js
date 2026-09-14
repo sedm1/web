ce4f60090f192a58078a68497afb293a0f36df61
sedmi <nikolaj.novikov@topvisor.com>
2026-09-14 22:59:45 +0300

    3

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.enable('strict routing');
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
+});
+
+app.get('/', (req, res) => {
+    res.type('text/plain').send('zefirnaya');
 });
 
 app.get('/login/', (req, res) => {
@@ -12,9 +18,36 @@ app.get('/login/', (req, res) => {
     res.end('zefirnaya');
 });
 
-app.get('/sample/', (req, res) => {
+app.get('/promise/', (req, res) => {
     res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-    res.end('function task(x) { return x * this ** 2; }');
+    res.end("function task(x){ return x < 18 ? Promise.resolve('yes') : Promise.reject('no'); }");
+});
+
+app.get('/fetch/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+    res.end(`<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Fetch</title>
+</head>
+<body>
+    <input id="inp">
+    <button id="bt">Fetch</button>
+    <script>
+        const inp = document.getElementById('inp');
+        const bt = document.getElementById('bt');
+
+        bt.addEventListener('click', () => {
+            fetch(inp.value)
+                .then(response => response.text())
+                .then(text => {
+                    inp.value = text;
+                });
+        });
+    </script>
+</body>
+</html>`);
 });
 
 
