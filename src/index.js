2caa979b2b7767d7bb23e06c8cdf2bbde4e6fb91
sedmi <nikolaj.novikov@topvisor.com>
2026-09-14 23:06:54 +0300

    6

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    const user = req.query.user || '';
-    res.send(`<h1>Привет, ${user}!</h1>`);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
 });
 
-app.get('/login', (req, res) => {
+app.get('/140926', (req, res) => {
     res.type('text/plain').send('zefirnaya');
+});
+
+app.get('/add/:x1/:x2', (req, res) => {
+    const result = Number(req.params.x1) + Number(req.params.x2);
+    res.type('text/plain').send(String(result));
+});
+
+app.get('/mpy/:y1/:y2', (req, res) => {
+    const result = Number(req.params.y1) * Number(req.params.y2);
+    res.type('text/plain').send(String(result));
 });
 
 
