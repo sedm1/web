a34efa469caf3745851643c2c6f91d430649695a
sedmi <nikolaj.novikov@topvisor.com>
2026-09-14 22:33:54 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
-app.get('/1', (req, res) => {
-    res.set('X-Author', 'zefirnaya')
-    res.set('Access-Control-Allow-Origin', '*')
-    res.send('zefirnaya');
+app.get('/', (req, res) => {
+    const user = req.query.user || '';
+    res.send(`<h1>Привет, ${user}!</h1>`);
+});
+
+app.get('/login', (req, res) => {
+    res.type('text/plain').send('zefirnaya');
 });
 
 app.listen(3000);
