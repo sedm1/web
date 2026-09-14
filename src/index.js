b606eed675870a83622e44d3ad1d61984f8853e8
sedmi <nikolaj.novikov@topvisor.com>
2026-09-14 22:41:57 +0300

    1

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    const user = req.query.user || '';
-    res.send(`<h1>Привет, ${user}!</h1>`);
+app.get('/id/:N', async (req, res) => {
+    try {
+        const response = await fetch(`https://nd.kodaktor.ru/users/${req.params.N}`);
+
+        if (!response.ok) {
+            return res.status(response.status).send('Ошибка внешнего сервиса');
+        }
+
+        const data = await response.json();
+
+        res.type('text/plain').send(data.login);
+    } catch (error) {
+        res.status(500).send('Ошибка сервера');
+    }
 });
 
 app.get('/login', (req, res) => {
