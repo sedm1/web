eaacc81f83604f09203987889cdfeedecaadaa2b
sedmi <nikolaj.novikov@topvisor.com>
2026-09-15 14:54:21 +0300

    1

--- src/index.js
+++ src/index.js
@@ -7,24 +7,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/login', (req, res) => {
+app.get('/', (req, res) => {
+    res.setHeader('X-Author', 'zefirnaya');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.type('text/plain').send('zefirnaya');
-});
-
-app.get('/id/:N', async (req, res) => {
-    try {
-        const response = await fetch(`https://nd.kodaktor.ru/users/${req.params.N}`);
-
-        if (!response.ok) {
-            return res.status(response.status).type('text/plain').send('error');
-        }
-
-        const data = await response.json();
-
-        res.type('text/plain').send(data.login);
-    } catch (error) {
-        res.status(500).type('text/plain').send('error');
-    }
 });
 
 
