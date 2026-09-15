753fed0d3a4e630e4f0bf632a813cb8e7c9553c7
sedmi <nikolaj.novikov@topvisor.com>
2026-09-15 14:52:01 +0300

    0

--- src/index.js
+++ src/index.js
@@ -7,13 +7,24 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => {
-    const user = req.query.user || '';
-    res.type('html').send(`<h1>Привет, ${user}!</h1>`);
-});
-
 app.get('/login', (req, res) => {
     res.type('text/plain').send('zefirnaya');
+});
+
+app.get('/id/:N', async (req, res) => {
+    try {
+        const response = await fetch(`https://nd.kodaktor.ru/users/${req.params.N}`);
+
+        if (!response.ok) {
+            return res.status(response.status).type('text/plain').send('error');
+        }
+
+        const data = await response.json();
+
+        res.type('text/plain').send(data.login);
+    } catch (error) {
+        res.status(500).type('text/plain').send('error');
+    }
 });
 
 
