d2849bcf058b96241aa819ecc5ed18a3ee3e989e
sedmi <nikolaj.novikov@topvisor.com>
2026-09-18 00:02:55 +0300

    4

--- src/index.js
+++ src/index.js
@@ -4,40 +4,23 @@ const app = express();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'x-test,ngrok-skip-browser-warning,Content-Type,Accept,Access-Control-Allow-Headers'
+    );
     next();
 });
 
-app.get('/login/', (req, res) => {
-    res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-    res.end('zefirnaya');
-});
+app.use(express.text({ type: '*/*' }));
 
-app.get('/promise/', (req, res) => {
-    res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-    res.end("function task(x){ return new Promise((resolve, reject) => x < 18 ? resolve('yes') : reject('no')); }");
-});
-
-app.get('/fetch/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-    res.end(`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Fetch</title>
-</head>
-<body>
-    <input id="inp">
-    <button id="bt">Fetch</button>
-    <script>
-        const inp = document.getElementById('inp');
-        document.getElementById('bt').addEventListener('click', () => {
-            fetch(inp.value)
-                .then(response => response.text())
-                .then(result => { inp.value = result; });
-        });
-    </script>
-</body>
-</html>`);
+app.all('/result4/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        message: 'zefirnaya',
+        'x-result': req.get('x-test'),
+        'x-body': req.body ?? ''
+    }));
 });
 
 app.listen(3000, '0.0.0.0');
