278c03d92d5b9cff8836c5d76adfbec2da3c0013
sedmi <nikolaj.novikov@topvisor.com>
2026-09-21 22:54:53 +0300

    Правка

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const app = express();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'x-test,ngrok-skip-browser-warning,Content-Type,Accept,Access-Control-Allow-Headers'
+    );
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+
     next();
 });
 
